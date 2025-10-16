671dd13f542b5de01a6e37b1ff4cc14c4fecfa11
lvan100 <lvan100@yeah.net>
2025-10-16 19:34:31 +0800

    111

--- gen/generator/golang/client/proto.go
+++ gen/generator/golang/client/proto.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 The Go-Spring Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package client
 
 import (
@@ -7,20 +23,28 @@ import (
 )
 
 var (
+	// Form provides an implementation of the Protocol interface using
+	// application/x-www-form-urlencoded encoding.
 	Form Protocol = &FormProtocol{}
+
+	// Json provides an implementation of the Protocol interface using
+	// JSON encoding.
 	Json Protocol = &JsonProtocol{}
 )
 
-// Protocol defines a common interface for encoding and decoding data.
+// Protocol defines a common interface for encoding and decoding data
+// into different serialization formats.
 type Protocol interface {
 	Encode(v any) ([]byte, error)
 	Decode(data []byte, v any) error
 }
 
-// FormProtocol serializes data into application/x-www-form-urlencoded format.
+// FormProtocol implements the Protocol interface for the
+// application/x-www-form-urlencoded format.
 type FormProtocol struct{}
 
-// Encode converts a struct or map into a form-encoded byte slice.
+// Encode serializes the given value into an application/x-www-form-urlencoded
+// encoded byte slice.
 func (p *FormProtocol) Encode(i any) ([]byte, error) {
 	if i == nil {
 		return []byte(""), nil
@@ -38,7 +62,7 @@ func (p *FormProtocol) Encode(i any) ([]byte, error) {
 
 	u := url.Values{}
 	for k, v := range m {
-		if v[0] == '"' {
+		if len(v) > 0 && v[0] == '"' {
 			s, err := strconv.Unquote(string(v))
 			if err != nil {
 				return nil, err
@@ -52,20 +76,21 @@ func (p *FormProtocol) Encode(i any) ([]byte, error) {
 	return []byte(u.Encode()), nil
 }
 
-// Decode parses form-encoded data into the given struct or map.
+// Decode deserializes application/x-www-form-urlencoded data into the
+// provided struct or map.
 func (p *FormProtocol) Decode(data []byte, v any) error {
 	panic("not implemented")
 }
 
-// JsonProtocol implements the JSON encoding protocol.
+// JsonProtocol implements the Protocol interface using JSON encoding.
 type JsonProtocol struct{}
 
-// Encode serializes the input value into JSON format.
+// Encode serializes the given value into a JSON-encoded byte slice.
 func (p *JsonProtocol) Encode(v any) ([]byte, error) {
 	return json.Marshal(v)
 }
 
-// Decode deserializes JSON data into the given value.
+// Decode deserializes JSON data into the provided value.
 func (p *JsonProtocol) Decode(data []byte, v any) error {
 	return json.Unmarshal(data, v)
 }
