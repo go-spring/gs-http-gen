6f1f89732bcf4345d459dbbaedf36a2ebfd21ba7
lvan100 <lvan100@yeah.net>
2025-10-16 19:26:27 +0800

    111

--- gen/generator/golang/client/proto.go
+++ gen/generator/golang/client/proto.go
@@ -2,6 +2,8 @@ package client
 
 import (
 	"encoding/json"
+	"net/url"
+	"strconv"
 )
 
 var (
@@ -9,30 +11,61 @@ var (
 	Json Protocol = &JsonProtocol{}
 )
 
-// Protocol 协议接口
+// Protocol defines a common interface for encoding and decoding data.
 type Protocol interface {
 	Encode(v any) ([]byte, error)
 	Decode(data []byte, v any) error
 }
 
-// FormProtocol 将内容序列话成 form 格式
+// FormProtocol serializes data into application/x-www-form-urlencoded format.
 type FormProtocol struct{}
 
-func (p *FormProtocol) Encode(v any) ([]byte, error) {
-	panic("not implemented")
+// Encode converts a struct or map into a form-encoded byte slice.
+func (p *FormProtocol) Encode(i any) ([]byte, error) {
+	if i == nil {
+		return []byte(""), nil
+	}
+
+	b, err := Json.Encode(i)
+	if err != nil {
+		return nil, err
+	}
+
+	var m map[string]json.RawMessage
+	if err = Json.Decode(b, &m); err != nil {
+		return nil, err
+	}
+
+	u := url.Values{}
+	for k, v := range m {
+		if v[0] == '"' {
+			s, err := strconv.Unquote(string(v))
+			if err != nil {
+				return nil, err
+			}
+			u.Set(k, s)
+		} else {
+			u.Set(k, string(v))
+		}
+	}
+
+	return []byte(u.Encode()), nil
 }
 
+// Decode parses form-encoded data into the given struct or map.
 func (p *FormProtocol) Decode(data []byte, v any) error {
 	panic("not implemented")
 }
 
-// JsonProtocol json 编码协议
+// JsonProtocol implements the JSON encoding protocol.
 type JsonProtocol struct{}
 
+// Encode serializes the input value into JSON format.
 func (p *JsonProtocol) Encode(v any) ([]byte, error) {
 	return json.Marshal(v)
 }
 
+// Decode deserializes JSON data into the given value.
 func (p *JsonProtocol) Decode(data []byte, v any) error {
 	return json.Unmarshal(data, v)
 }
