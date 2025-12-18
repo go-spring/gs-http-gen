e7bad4096aa3435e527a8079e318de0a219d56f8
lvan100 <lvan100@yeah.net>
2025-12-18 14:01:59 +0800

    111

--- lib/jsonutil/decode.go
+++ lib/jsonutil/decode.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/json/jsontext"
 	"errors"
+	"math"
 	"strconv"
 
 	"github.com/lvan100/golib/errutil"
@@ -162,43 +163,114 @@ func DecodeKey(d Decoder) (string, error) {
 
 // ParseBool ...
 func ParseBool(s string, k Kind) (bool, error) {
+	if k != 'f' && k != 't' {
+		return false, errutil.Explain(nil, "invalid JSON: expected boolean")
+	}
 	return strconv.ParseBool(s)
+}
+
+// OverflowInt ...
+func OverflowInt[T int | int8 | int16 | int32 | int64](v int64) bool {
+	var z T
+	switch any(z).(type) {
+	case int:
+		return v > math.MaxInt || v < math.MinInt
+	case int8:
+		return v > math.MaxInt8 || v < math.MinInt8
+	case int16:
+		return v > math.MaxInt16 || v < math.MinInt16
+	case int32:
+		return v > math.MaxInt32 || v < math.MinInt32
+	case int64:
+		return v > math.MaxInt64 || v < math.MinInt64
+	}
+	return false
 }
 
 // ParseInt ...
 func ParseInt[T int | int8 | int16 | int32 | int64](s string, k Kind) (T, error) {
+	if k != '0' {
+		return 0, errutil.Explain(nil, "invalid JSON: expected number")
+	}
 	v, err := strconv.ParseInt(s, 10, 64)
 	if err != nil {
 		return 0, err
 	}
+	if OverflowInt[T](v) {
+		return 0, errutil.Explain(nil, "invalid JSON: number out of range")
+	}
 	return T(v), nil
+}
+
+// OverflowUint ...
+func OverflowUint[T uint | uint8 | uint16 | uint32 | uint64](v uint64) bool {
+	var z T
+	switch any(z).(type) {
+	case uint:
+		return v > math.MaxUint
+	case uint8:
+		return v > math.MaxUint8
+	case uint16:
+		return v > math.MaxUint16
+	case uint32:
+		return v > math.MaxUint32
+	}
+	return false
 }
 
 // ParseUint ...
 func ParseUint[T uint | uint8 | uint16 | uint32 | uint64](s string, k Kind) (T, error) {
-	u, err := strconv.ParseUint(s, 10, 64)
+	if k != '0' {
+		return 0, errutil.Explain(nil, "invalid JSON: expected number")
+	}
+	v, err := strconv.ParseUint(s, 10, 64)
 	if err != nil {
 		return 0, err
 	}
-	return T(u), nil
+	if OverflowUint[T](v) {
+		return 0, errutil.Explain(nil, "invalid JSON: number out of range")
+	}
+	return T(v), nil
+}
+
+// OverflowFloat ...
+func OverflowFloat[T float32 | float64](v float64) bool {
+	var z T
+	switch any(z).(type) {
+	case float32:
+		return v > math.MaxFloat32 || v < -math.MaxFloat32
+	}
+	return false
 }
 
 // ParseFloat ...
 func ParseFloat[T float32 | float64](s string, k Kind) (T, error) {
+	if k != '0' {
+		return 0, errutil.Explain(nil, "invalid JSON: expected number")
+	}
 	f, err := strconv.ParseFloat(s, 64)
 	if err != nil {
 		return 0, err
+	}
+	if OverflowFloat[T](f) {
+		return 0, errutil.Explain(nil, "invalid JSON: number out of range")
 	}
 	return T(f), nil
 }
 
 // ParseString ...
 func ParseString(s string, k Kind) (string, error) {
+	if k != '"' {
+		return "", errutil.Explain(nil, "invalid JSON: expected string")
+	}
 	return s, nil
 }
 
 // ParseBytes ...
 func ParseBytes(s string, k Kind) ([]byte, error) {
+	if k != '"' {
+		return nil, errutil.Explain(nil, "invalid JSON: expected string")
+	}
 	return base64.StdEncoding.DecodeString(s)
 }
 
